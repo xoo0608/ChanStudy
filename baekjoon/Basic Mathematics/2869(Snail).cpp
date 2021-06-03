ee34f2145c36f47cb7d036b6d977e3cecdcbb3ee
xoo0608 <xoo0608@gmail.com>
2021-06-03 22:07:05 +0900

    Sugar delivery

--- baekjoon/Basic Mathematics/2869(Snail).cpp
+++ baekjoon/Basic Mathematics/2869(Snail).cpp
@@ -1,16 +1,26 @@
-//Snail
+//Sugar delivery
 #include<iostream>
 using namespace std;
 
 int main()
 {
-    int A, B, V, ans;
-    cin>>A>>B>>V;
+    int A, B, input, ans = -1;
     
-    if((V - A) % (A - B) == 0) ans = (V - A)/(A - B);
-    else ans = (V - A)/(A - B) + 1;
+    cin>>input;
 
-    cout<<ans + 1; //1일 부터 시작
+    A = input / 5; //5kg의 갯수를 최대 가능한 수로 정한다
+
+    for(A;A>=0;A--) //5kg의 수를 줄여가면서 조건을 만족하는 3kg의 수를 찾는다
+    {
+        if((input - (A * 5)) % 3 == 0) //5kg으로 제외된 남은 양이 3kg로 나누어 떨어지면 조건 만족
+        {
+            B = (input - (A * 5)) / 3;
+            ans = A + B;
+            break;
+        }
+    }
+
+    cout<<ans;
 
     return 0;
 
