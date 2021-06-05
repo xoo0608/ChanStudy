79dc4c3d9c55e9487d03c4daa83d95838077b990
xoo0608 <xoo0608@gmail.com>
2021-06-05 17:30:30 +0900

    Prime number3_sqare root(Failed_time out)

---
+++ baekjoon/Basic Mathematics/1929_F(Prime number3_sqare root).cpp
@@ -0,0 +1,31 @@
+//Prime number3_sqare root
+//제곱근를 활용한 알고리즘(시간초과)
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+bool isprimenum(int n)
+{
+    if(n == 2) return true;
+    if(n % 2 == 0 || n == 1) return false;
+
+    for(int i = 3 ; i <= (int)sqrt(double(n)) ; i +=2)
+    {
+        if(n % i == 0) return false;
+    }
+    return true;
+}
+
+int main()
+{
+    int num1, num2;
+    cin>>num1>>num2;
+
+    for(int i = num1 ; i <= num2 ; i++)
+    {
+        if(isprimenum(i)) cout<<i<<endl;
+    }
+
+    return 0;
+
+}
