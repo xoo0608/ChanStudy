1612f18ac93c3d0ff397415f394e026caa316135
xoo0608 <xoo0608@gmail.com>
2021-06-06 23:05:20 +0900

    Right triangle

---
+++ baekjoon/Basic Mathematics/4153(Right triangle).cpp
@@ -0,0 +1,22 @@
+//
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    while(true)
+    {
+        int a, b, c;
+
+        cin>>a>>b>>c;
+
+        if(a == 0 && b == 0 && c == 0) break;
+
+        if((a * a) + (b * b) == (c * c) || (a * a) + (c * c) == (b * b) || (c * c) + (b * b) == (a * a)) cout<<"right"<<endl;
+        else cout<<"wrong"<<endl;
+    }
+
+    return 0;
+
+}
