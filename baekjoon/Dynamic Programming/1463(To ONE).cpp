ece6c8dea29fc0ec0de28b1b275c6ea2f1607767
xoo0608 <xoo0608@gmail.com>
2021-06-09 23:26:27 +0900

    To ONE

---
+++ baekjoon/Dynamic Programming/1463(To ONE).cpp
@@ -0,0 +1,23 @@
+//To ONE
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[1000001] = {0, 0};
+
+int main()
+{
+    int input;
+    cin>>input;
+
+    for(int i = 2 ; i <= input ; i++)
+    {
+        dp[i] = dp[i - 1] + 1;
+        if (i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1);
+        if (i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1);
+    }
+
+    cout<<dp[input]<<endl;
+
+    return 0;
+}
