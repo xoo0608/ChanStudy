e991d753b900c8ef70d7cd5473d7dd8c5a60cd55
xoo0608 <xoo0608@gmail.com>
2021-06-26 21:51:56 +0900

    Card2

---
+++ CPPStudy/baekjoon/Queue and Deque/2164(Card2).cpp
@@ -0,0 +1,34 @@
+//Card2
+#include <iostream>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    queue<int> q;
+    int input;
+
+    cin>>input;
+
+    for(int i = 1 ; i <= input ; i++) q.push(i);
+
+    while(true)
+    {
+        if(q.size() == 1)
+        {
+            cout<<q.front()<<endl;
+            break;
+        }
+        else
+        {
+            q.pop();
+            q.push(q.front());
+            q.pop();
+        }
+
+    }
+
+    return 0;
+}
