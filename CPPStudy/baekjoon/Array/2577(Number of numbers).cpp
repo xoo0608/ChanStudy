16a8ca5ed660f7ca4ef83c88fcc672da40f713fc
xoo0608 <xoo0608@gmail.com>
2021-07-12 21:47:48 +0900

    Number of numbers

---
+++ CPPStudy/baekjoon/Array/2577(Number of numbers).cpp
@@ -0,0 +1,23 @@
+//
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int a, b, c;
+    int arr[10] = {0};
+    
+    cin>>a>>b>>c;
+    
+    int ans = a * b * c;
+
+    while(ans != 0)
+    {
+        arr[ans % 10] += 1;
+        ans = ans / 10;
+    }
+    
+    for(int i = 0 ; i < 10 ; i++) cout<<arr[i]<<endl;
+    
+    return 0;
+}
