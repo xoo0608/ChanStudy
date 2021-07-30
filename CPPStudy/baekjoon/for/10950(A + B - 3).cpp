a5729574f87654753054bbcbb72836683b5ecb57
xoo0608 <xoo0608@gmail.com>
2021-07-30 12:32:41 +0900

    A + B - 3

---
+++ CPPStudy/baekjoon/for/10950(A + B - 3).cpp
@@ -0,0 +1,16 @@
+//A + B - 3
+#include <iostream>
+using namespace std;
+
+int main(){
+    int T;
+    cin>>T;
+
+    for(int i = 0 ; i < T ; i++){
+        int n1, n2;
+        cin>>n1>>n2;
+        cout<<n1 + n2<<endl;
+    }
+
+    return 0;
+}
